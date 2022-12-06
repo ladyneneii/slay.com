a4a7d5d375cfe0c4eb0e09cd1ff0e693e838113e
Nene <ernestcurativo@gmail.com>
2022-12-06 16:33:34 +0800

    Updated 200 to 100

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
             <article className="about__card">
               <FiUsers className="about__icon"/>
               <h5>Clients</h5>
-              <small>200+ Worldwide</small>
+              <small>100+ Worldwide</small>
             </article>
 
             <article className="about__card">
