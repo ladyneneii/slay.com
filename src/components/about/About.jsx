cd463b13c9f34b90d4b253f0240dbd3e97c613c8
Nene <ernestcurativo@gmail.com>
2022-12-06 19:21:04 +0800

    Added slay if u wanna slay

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
           </div>
 
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt adipisci maiores earum commodi, aut possimus voluptatem provident doloremque voluptate reprehenderit laboriosam ipsam ducimus fugit deleniti veniam libero, ut incidunt!
+            Slay if u wanna slay, consectetur adipisicing elit. Accusantium deserunt adipisci maiores earum commodi, aut possimus voluptatem provident doloremque voluptate reprehenderit laboriosam ipsam ducimus fugit deleniti veniam libero, ut incidunt!
           </p>
 
           <a href="#contact" className="btn btn-primary">Let's Talk</a>
